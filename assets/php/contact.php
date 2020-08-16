83effe5e610d1dc2b60861ac5cb55fcbaa09b954
Gwendoline Sanabria <gwensanabria@gmail.com>
2020-08-16 00:30:12 -0500

    edit php

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,34 +1,46 @@
 <?php
-$field_name = $_POST['name'];
-$field_email = $_POST['email'];
-$field_subject = $_POST['subject'];
-$field_message = $_POST['message'];
 
-$mail_to = 'gwensanabria@gmail.com';
-$subject = 'Your Subject'.$field_name;
+	if(isset($_POST['email']) && $_POST['email'] != '') {
 
-$body_message = 'From: '.$field_name."\n";
-$body_message .= 'E-mail: '.$field_email."\n";
-$body_message .= 'Message: '.$field_subject;
-$body_message .= 'Number: '.$field_message;
+		if( filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
+			// Submit Form
+			$field_name = $_POST['name'];
+			$field_email = $_POST['email'];
+			$field_subject = $_POST['subject'];
+			$field_message = $_POST['message'];
 
-$headers = 'From: '.$field_email."\r\n";
-$headers .= 'Reply-To: '.$field_email."\r\n";
+			$mail_to = 'gwensanabria@gmail.com';
+			// $subject = 'Your Subject'.$field_name;
+			$body_message = "";
 
-$mail_status = mail($mail_to, $subject, $body_message, $headers);
 
-if ($mail_status) { ?>
-	<script language="javascript" type="text/javascript">
-		alert('Message Sent');
-		window.location = 'index.html';
-	</script>
-<?php
-}
-else { ?>
-	<script language="javascript" type="text/javascript">
-		alert('Error Message');
-		window.location = 'index.html';
-	</script>
-<?php
-}
+			$body_message .= 'From: '.$field_name."\n";
+			$body_message .= 'E-mail: '.$field_email."\n";
+			$body_message .= 'Message: '.$field_subject;
+			$body_message .= 'Number: '.$field_message;
+
+			$headers = 'From: '.$field_email."\r\n";
+			$headers .= 'Reply-To: '.$field_email."\r\n";
+
+			$mail_status = mail($mail_to, $field_subject, $body_message, $headers);
+
+			if ($mail_status) { ?>
+				<script language="javascript" type="text/javascript">
+					alert('Message Sent');
+					window.location = 'index.html';
+				</script>
+			<?php
+			}
+			else { ?>
+				<script language="javascript" type="text/javascript">
+					alert('Error Message');
+					window.location = 'index.html';
+				</script>
+			<?php
+			}
+		}
+
+
+	}
+
 ?>
